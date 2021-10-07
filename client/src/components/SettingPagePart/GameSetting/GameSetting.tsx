5d5925da84bc61766f43171d262bfaf92c5c4a9e
Anna Guzova <36640991+anutaguzova@users.noreply.github.com>
2021-10-07 18:47:57 +0300

    fix ui settings

--- client/src/components/SettingPagePart/GameSetting/GameSetting.tsx
+++ client/src/components/SettingPagePart/GameSetting/GameSetting.tsx
@@ -24,13 +24,17 @@ const GameSetting = (): JSX.Element => {
       wrapperCol={{ span: 10 }}
       layout="horizontal"
       size="middle"
-      style={{ width: '80%', display: 'flex', justifyContent: 'space-evenly' }}
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
     >
       <Col span={10}>
-        <FormItem label="Scram master as player:" valuePropName="checked">
+        <FormItem label="Scram master as player" valuePropName="checked">
           <Switch onChange={(checked) => dispatch(setMasterPlayer(checked))} />
         </FormItem>
-        <FormItem label="Changing card in round end:" valuePropName="checked">
+        <FormItem label="Changing card in round end" valuePropName="checked">
           <Switch onChange={(checked) => dispatch(setChangingCard(checked))} />
         </FormItem>
         <FormItem label="Is timer needed:" valuePropName="checked">
@@ -38,7 +42,7 @@ const GameSetting = (): JSX.Element => {
         </FormItem>
       </Col>
       <Col span={10} style={{ paddingRight: '20px' }}>
-        <FormItem labelCol={{ span: 10 }} label="Score type:">
+        <FormItem labelCol={{ span: 10 }} label="Score type">
           <Input onChange={(e) => dispatch(setScoreType(e.target.value))} />
         </FormItem>
         <FormItem labelCol={{ span: 10 }} label="Score type (Short)">
@@ -47,7 +51,7 @@ const GameSetting = (): JSX.Element => {
             onChange={(e) => dispatch(setShortScoreType(e.target.value))}
           />
         </FormItem>
-        <FormItem labelCol={{ span: 10 }} name="slider" label="Round time:">
+        <FormItem labelCol={{ span: 10 }} name="slider" label="Round time">
           <Slider
             trackStyle={{ width: '200px' }}
             max={1000}
